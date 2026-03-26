6c165918b6ac6efc22720fa9582e8fca62db502f
hiduard <eduogassa@gmail.com>
2026-03-26 15:27:16 -0300

    funcoes

--- compilador.py
+++ compilador.py
@@ -9,3 +9,44 @@
 # Grupo 17
 #
 # ============================================================
+
+def estadoInicial(linha, pos, tokens):
+
+def estadoNumero(linha, pos, tokens):
+
+def estadoOperador(linha, pos, tokens):
+
+def estadoDivisao(linha, pos, tokens):
+
+def estadoPalavraChave(linha, pos, tokens):
+
+def estadoParenteses(linha, pos, tokens):
+
+def parseExpressao(linha, tokens):
+
+def _potencia_inteira(base, expoente):
+
+def _truncar(valor):
+
+def executarExpressao(tokens, resultados, memoria):
+
+def _avaliar_subexpressao(contexto, resultados, memoria):
+
+def _gerar_operacoes_intermediarias(tokens, resultados_hist, memoria_nomes):
+
+def _processar_subexpr_asm(contexto, resultados_hist, memoria_nomes):
+
+def gerarAssembly(tokens, codigoAssembly):
+
+def exibirResultados(resultados):
+
+def testeProgramaCompleto(nomeArquivo):
+
+def lerArquivo(nomeArquivo, linhas):
+
+def salvarTokens(tokens_por_linha, nomeArquivo):
+
+def main():
+
+if _name_ == "_main_":
+    main()
