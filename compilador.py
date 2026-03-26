15e4460839456c0332f093b39ab104da922d350b
hiduard <eduogassa@gmail.com>
2026-03-26 14:57:47 -0300

    cabeçalho, precisa add id do GitHub

---
+++ compilador.py
@@ -0,0 +1,11 @@
+# ============================================================
+# Analisador Léxico e Gerador de Código Assembly ARMv7
+# Projeto de Compiladores - Fase 1
+#
+# Integrantes do grupo:
+#   Eduardo Hideo Itinoseke Ogassawara
+#   Gabriel Barbosa Fernandes de Oliveira
+#
+# Grupo 17
+#
+# ============================================================
